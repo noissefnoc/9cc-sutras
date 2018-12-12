2f1125ce0ee3c59425e84f34378bb4a6df4a5fc0
Kota Saito <noissefnoc@gmail.com>
2018-12-12 18:08:11 +0900

    Fix warnings

--- 9cc.c
+++ 9cc.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdnoreturn.h>
 #include <string.h>
 
 // Tokenizer
@@ -88,7 +89,7 @@ void tokenize(char *p) {
 }
 
 // An error reporting function.
-void error(char *fmt, ...) {
+noreturn void error(char *fmt, ...) {
     va_list ap;
     va_start(ap, fmt);
     vfprintf(stderr, fmt, ap);
@@ -164,7 +165,7 @@ int gen_ir_sub(Node *node) {
     return lhs;
 }
 
-int gen_ir(Node *node) {
+void gen_ir(Node *node) {
     int r = gen_ir_sub(node);
     ins[inp++] = new_ir(IR_RETURN, r, 0);
 }
