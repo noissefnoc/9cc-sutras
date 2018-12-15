203d4a6699b093b17a8a0422aca68d45cd8abece
noissefnoc <noissefnoc@gmail.com>
2018-12-15 22:49:52 +0900

    Split into multiple .c files.

--- test.sh
+++ test.sh
@@ -4,7 +4,6 @@ try() {
     expected="$1"
     input="$2"
 
-    gcc -static -o 9cc 9cc.c
     ./9cc "$input" > tmp.s
     gcc -static -o tmp tmp.s
     ./tmp
@@ -17,6 +16,8 @@ try() {
         exit 1
     fi
 }
+
+make -f Makefile.linux
 
 try 0 0
 try 42 42
